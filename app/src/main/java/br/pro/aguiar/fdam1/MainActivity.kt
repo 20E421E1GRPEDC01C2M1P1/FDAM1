cc5175557ebbcbba0204c399f2674681cf7c4595
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-10-29 07:50:47 -0300

    Transições: Intent e StartActivity.

--- app/src/main/java/br/pro/aguiar/fdam1/MainActivity.kt
+++ app/src/main/java/br/pro/aguiar/fdam1/MainActivity.kt
@@ -1,11 +1,29 @@
 package br.pro.aguiar.fdam1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAcessar.setOnClickListener {
+            var intent = Intent(this,
+                    HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCadastrarse.setOnClickListener {
+            var intent = Intent(
+                    this,
+                    CadastroActivity::class.java
+            )
+            startActivity(intent)
+        }
+
     }
 }
