268865bb583b643b3521883c3e241fc15764eb8e
Thiago Vieira de Aguiar <thiagoaguiar@tic.ufrj.br>
2020-11-24 08:51:47 -0300

    RecyclerView: Widget RecyclerView (XML), Classe Modelo (Carro), Layout de item (XML), RecyclerAdapter (CarroRecyclerAdapter).

--- app/src/main/java/br/pro/aguiar/fdam1/MainViewModel.kt
+++ app/src/main/java/br/pro/aguiar/fdam1/MainViewModel.kt
@@ -4,8 +4,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.pro.aguiar.fdam1.carro.model.Carro
+import java.lang.reflect.Field
 
 class MainViewModel:  ViewModel() {
+
+    // mainViewModel.nome
+    // mainViewModel.nome = "Thiago"
+//    var nome: String
+//        get() = "Nome: $nome"
+//        set(value) { field = value }
+//    var usuario: Usuario
+//    usuario.nome
+//    usuario.pontuacao
+//    usuario.questoes
 
     private val _carro = MutableLiveData<Carro>()
     val carro: LiveData<Carro>
---
+++ app/src/main/java/br/pro/aguiar/fdam1/carro/adapter/CarroRecyclerAdapter.kt
@@ -0,0 +1,47 @@
+package br.pro.aguiar.fdam1.carro.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import br.pro.aguiar.fdam1.R
+import br.pro.aguiar.fdam1.carro.model.Carro
+import kotlinx.android.synthetic.main.list_carro_recycler.view.*
+
+class CarroRecyclerAdapter(
+  private val carros: List<Carro>
+) : RecyclerView.Adapter<CarroRecyclerAdapter.CarroViewHolder>() {
+
+    class CarroViewHolder(view: View)
+        : RecyclerView.ViewHolder(view) {
+        val textViewModelo = view.textViewListCarroModelo
+        val textViewMarca = view.textViewListCarroMarca
+        val textViewPlaca = view.textViewListCarroPlaca
+        val textViewQuantidadeOpiniao = view.textViewListCarroQuantidadeOpiniao
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): CarroViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(
+                R.layout.list_carro_recycler,
+                parent,
+                false)
+        val carroViewHolder = CarroViewHolder(view)
+        return carroViewHolder
+    }
+
+    override fun onBindViewHolder(holder: CarroViewHolder, position: Int) {
+        val carro = carros.get(position)
+        holder.textViewModelo.text = carro.modelo
+        holder.textViewMarca.text = carro.marca
+        holder.textViewPlaca.text = carro.placa
+        holder.textViewQuantidadeOpiniao.text = carro.opinioes?.size.toString()
+    }
+
+    override fun getItemCount(): Int = carros.size
+
+}
